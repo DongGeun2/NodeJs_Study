58dac0158170c38d23b2ec26a5e328330779aef3
ehdrms519@naver.com <ehdrms519@naver.com>
2021-07-29 16:19:59 +0900

    QueryData

--- main.js
+++ main.js
@@ -1,24 +1,46 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-const { query } = require('express');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var QueryData = url.parse(_url, true).query;
-    console.log(_url);
-    console.log(QueryData.id)
+    var title = QueryData.id;
+    
 
     if(_url == '/'){
-        _url = '/views/index.html';
+        title = 'welcome';
     }
 
     if(_url == "/favicon.ico"){
         return response.writeHead(404);
     }
     response.writeHead(200);
-    response.end(QueryData.id);
+    fs.readFile(`data/${QueryData.id}`, `UTF8`, function(err, description){
 
+        var template = `
+        <!doctype html>
+        <html>
+        <head>
+          <title>WEB1 - ${title}</title>
+          <meta charset="utf-8">
+        </head>
+        <body>
+          <h1><a href="/">WEB</a></h1>
+          <ul>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+          </ul>
+          <h2>${title}</h2>
+          <p>${description}</p>
+        </body>
+        </html>
+        `;
+        
+        response.end(template);
+
+    });
 
 });
 
